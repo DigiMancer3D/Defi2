f02a1d93533da297d319f44356b4bcfb9eda9f87
3Douglas <3dpihl@gmail.com>
2020-09-07 11:59:15 -0500

    Create devpool.js

---
+++ api/devpool.js
@@ -0,0 +1,64 @@
+/*
+************
+  Credits
+------------
+//Defi 2.0 Initiator: 3Douglas "3D" (git: Digimancer3D)
+//
+//    Developers::
+//         Sr Dev: KrazyKewl B "B roll" (git: KrazyKewl)
+//         Jr Dev: 3Douglas "3D" (git: Digimancer3D)
+//         Sr Tron Dev: KrazyKewl B "B roll" (git: KrazyKewlB)
+//         Jr Tron Dev: **Position Open**
+//         Sr Ethereum Dev: **Position Open**
+//         Jr Etherem Dev: **Position Open**
+//         Sr Skycoin Dev: **Position Open**
+//         Jr Skycoin Dev: **Position Open**
+//         Sr EOS Dev: **Position Open**
+//         Jr EOS Dev: **Position Open**
+//                    **Not all positions need to be filled**
+//
+//Defi 2.0 Community Managers deserve Pay too!
+//
+//    Artists::
+//         Graphics: ''Earmarked::Ricou "Ricooouuuu" (git: --N/A--)''
+//         Logo: ''Earmarked::Dexter "Hog Dexter" (git: --N/A--)''
+//    
+//    Community Managers::
+//         Africa Manager(s): Kit U "Kit BTC" (git: --N/A--)
+//                             **Position Open**
+//         North America Manager(s): AP "Dr. Me" (git: --N/A--)
+//                         **Position Open**
+//         Erupoean Managers(s): **Position Open**
+//                               **Position Open**
+//         South America Manager(s): **Position Open**
+//                                   **Position Open**
+//         Asia Manager(s): **Position Open**
+//                          **Position Open**
+//         Antartica Manager(s): **Position Open**
+//                               **Position Open**
+//         Oceania Manager(s): **Position Open**
+//                             **Position Open**
+//                    **Not all positions need to be filled**
+//
+//   Thank you everyone for your support and help in making this Defi Project the best ever!
+*/
+
+            //Tron Pool Addresses::
+
+
+
+
+            //Ethereum Pool Addresses::
+
+
+
+
+            //Sky Pool Addresses::
+
+
+
+
+
+
+
+//List Addresses above per token type
