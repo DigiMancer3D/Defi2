6ad6dcdce1545f7d886abd19fa49ca5cadb31cb7
3Douglas <3dpihl@gmail.com>
2020-09-11 00:15:41 -0500

    Update token.sol

--- contracts/token.sol
+++ contracts/token.sol
@@ -10,7 +10,7 @@ contract Test{
 
 //require poolCheck 
 
-var poolCheck = artifacts.require("./poolCheck.sol");
+var poolCheck = artifacts.require("./rePool.sol/poolCheck");
 var Burning = artifacts.require("./Burning.sol");
 var Minting = artifacts.require("./Minting.sol");
 
