0d63065ef4f441a158bf9f3825db4fdfc74e1c3e
3Douglas <3dpihl@gmail.com>
2020-09-14 22:16:39 -0500

    Update token.sol

--- contracts/token.sol
+++ contracts/token.sol
@@ -13,10 +13,42 @@ contract Test{
 var poolCheck = artifacts.require("./rePool.sol/poolCheck"); //needs to pull from /contracts/rePool.sol
 var Burning = artifacts.require("./Burning.sol"); //needs to exist still
 var Minting = artifacts.require("./Minting.sol"); //needs to exist still
+var BurnLen = [];
+BurnLen += artifacts.require("./Burning.sol");
+var burnlist = [];
 
 module.exports = function(deployer) {
   deployer.deploy(poolCheck);
   deployer.deploy(Burning);
   deployer.deploy(Minting);
 };
+
+//check burn wallet for minting math
+
+var burnnum = Burning.value;
+
+
+
+// check minting wallet for minting math
+  // looking for the number of unique wallet addresses
+  var mintList = artifacts.require("./Minting.sol").addresses;
+  sort(mintList);
+var mintsize = mintList;
+
+comp(burnnum, mintsize);
+//compare 
+
+comp(burnnum, mintsize) {
+if burnnum >= (mintsize*0.15) {
+  var burnnum2 = BurnLen.slice[BurnLen.length+1];
+  if burnnum2.length >= burnnum.length;
+  return burnnum = burnnum2;
+  comp(burnnum, mintsize); 
+}
+else {
+  burnlist = [burnnum];
+}
+return (burnnum, mintsize, burnlist);
+}
+
 
