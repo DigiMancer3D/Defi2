f6d54c8268989cfbe29918cea45551358adff393
3Douglas <3dpihl@gmail.com>
2020-09-14 22:30:53 -0500

    Create timers.js
    putting thoughts down as i look for options to do actions

---
+++ api/timers.js
@@ -0,0 +1,27 @@
+onLoad(timer1A());
+
+function timer1A() {
+ setInterval(timeOff, 388800000);
+ timeOn();
+}
+
+function timeOn() {
+ setTimeout(poolWait, 259200000);
+  //import sorted minting addresses from token, should be valued by percent by now (no more than 100%)
+  
+  //import sorted burning addresses from token/burning, should be valued be percent by now (no nore than 100%)
+  
+  //subtract burning considered addresses total from imported sorted minting addresses total
+  
+  //find total needed to mint, check is over network minimum, if not, send to mint minimum
+  
+  //mint token number result from mint-check over minimum 
+}
+
+
+funtion timeOff() {
+//import wallet to determine 10% (dev paybak) and 15% (burn pool)
+//pay dev & burn return in single transaction
+
+//pay minted tokens to sorted minting addresses, % per address sends % of total
+}
