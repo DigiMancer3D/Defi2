103cde26d53bdd115267f2e1a310af5be30333f3
KrazyB <61848950+KrazyKewlB@users.noreply.github.com>
2020-10-29 21:30:13 -0400

    Update contract-template.js

--- contract-template.js
+++ contract-template.js
@@ -11,6 +11,16 @@ module.exports = {
         //  eventServer:  "it is optional",
         //  network_id: "*" // Match any network id
       },
+     shasta: {//testnet
+          from: 'some other address',
+          privateKey: 'some other private key',
+          consume_user_resource_percent: 30,
+          fee_limit: 100000000,
+          fullNode: "https://api.shasta.trongrid.io",
+          solidityNode: "https://api.trongrid.io",
+      //    eventServer:  "it is optional",
+      //    network_id: "*" // Match any network id
+      },
       production: {
           from: 'some other address',
           privateKey: 'some other private key',
@@ -24,4 +34,4 @@ module.exports = {
     ..... you can define other network configuration as well
   }
 };
-//this template is not limited to only tron....the network config can be modified
+//this template is not limited to only tron....the network config can be modified will also add the solidity compiler version
